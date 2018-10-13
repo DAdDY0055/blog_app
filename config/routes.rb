4a9bf1946dee3709ad86f27827ecb6276be6fad0
Takuya_Nakahara <yky2800.rthm.55tn@gmail.com>
2018-10-13 16:57:10 +0900

    login change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 
-  get 'tops/index'
+  root to: 'tops#index' 
 
-  get '/', to: 'tops#index' 
+  get 'tops/index'
   
   resources :blogs do
     collection do
